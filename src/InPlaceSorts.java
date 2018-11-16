b92a37cf7aa400452f4f68adb9bc49165b71079b
BT_1N3_29 <vli8558@bths.edu>
2018-11-16 10:17:25 -0500

    ??

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -1,25 +1,55 @@
+import java.util.Arrays;
+
 public class InPlaceSorts {
+    public static void insertionSort(int[] arr)
+    {
+        int[] arrCopy = new int[arr.length];
+
+        for(int i=0;i<arr.length;i++) {
+            int check = 0;
+            check++;
+            arrCopy[i] = arr[i];
+            if (i != 0) {
+                int pos = i;
+                while (check > 0) {
+                    if(pos > 0) {
+                        if (arr[pos - check] > arr[pos]) {
+                            swap(arr, (pos - check), pos);
+                            pos--;
+                        } else {
+                            check = 0;
+                        }
+                    }
+                    if(pos == 0)
+                    {
+                        check =0;
+                    }
+                }
+            }
+        }
+
+    }
+
     public static void selectionSort(double[] list1)
     {
-        int posMin = 0;
-        int posb = 0;
-        double valMin = list1[0];
-        double valb = 0;
+        int posB =0;
+        double min =0;
         for(int z=0;z<list1.length;z++)
         {
-
-            for(int i=0;i<list1.length-1;i++)
+            posB = z;
+            min = list1[z];
+            for(int i=z;i<list1.length-1;i++)
             {
-                if(valMin > list1[i+1])
+                if(min > list1[i+1])
                 {
-                    valMin = list1[i+1];
-                    posMin = i+1;
+                   posB = i+1;
+                   min = list1[i+1];
                 }
 
 
             }
 
-            swap(list1,posb,posMin);
+            swap(list1,posB,z);
         }
     }
     public static void bubbleSort(String[] list1)
@@ -29,6 +59,12 @@ public class InPlaceSorts {
     public static void swap(double[] x,int y, int z)
     {
         double temp = x[z];
+        x[z] = x[y];
+        x[y] = temp;
+    }
+    public static void swap(int[] x,int y, int z)
+    {
+        int temp = x[z];
         x[z] = x[y];
         x[y] = temp;
     }
--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
     public static void main(String[] args)
     {
-        double[] y = new double[10];
+        double[] y = new double[100000];
         for(int i=0;i<y.length;i++)
         {
             y[i] = Math.random()*10;
@@ -9,16 +9,80 @@ public class Runner {
         String str = "";
         for(int i=0;i<y.length;i++)
         {
-            str += (int)(y[i]) + " ";
+            str += (y[i]) + " ";
         }
         System.out.println(str);
+        long tim = System.currentTimeMillis();
         InPlaceSorts.selectionSort(y);
-
+        System.out.println(System.currentTimeMillis()-tim);
         str = "";
         for(int i=0;i<y.length;i++)
         {
-            str += (int)(y[i]) + " ";
+            str += (y[i]) + " ";
         }
         System.out.println(str);
+
+
+
+
+
+
+
+        System.out.println("\n");
+        System.out.println("\n");
+
+        int[] x = new int[10000];
+        for(int i=0;i<x.length;i++)
+        {
+            x[i] = (int)(Math.random()*10);
+        }
+        str = "";
+        for(int i=0;i<y.length;i++)
+        {
+            str += (x[i]) + " ";
+        }
+        System.out.println(str);
+        long time = System.currentTimeMillis();
+        InPlaceSorts.insertionSort(x);
+        System.out.println(System.currentTimeMillis() - time);
+        str = "";
+        for(int i=0;i<y.length;i++)
+        {
+            str += (x[i]) + " ";
+        }
+
+        System.out.println(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    public String[] randomStringArr(int num, int length){
+        String[] arr = new String[num];
+        while(num>0)
+        {
+            int i=0;
+            String s = "";
+            while(i<length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s+c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
+
     }
 }
